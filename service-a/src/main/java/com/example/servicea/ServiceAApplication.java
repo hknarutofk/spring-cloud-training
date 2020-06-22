6f823a24509b6f419af6b7d52f6bf358e4511198
yeqiang <289632785@qq.com>
2020-06-22 19:56:08 +0800

    纠正swagger2

--- service-a/src/main/java/com/example/servicea/ServiceAApplication.java
+++ service-a/src/main/java/com/example/servicea/ServiceAApplication.java
@@ -6,10 +6,13 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableSwagger2
 public class ServiceAApplication {
 
     public static void main(String[] args) {
